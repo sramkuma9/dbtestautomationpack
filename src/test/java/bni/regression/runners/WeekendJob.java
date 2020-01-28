3f83adf9a519c4680c0d0593223100e5c86e261d
Srini <ajay.rsin@gmail.com>
2020-01-28 19:27:17 +0530

    Created a runner class for weekend job

---
+++ src/test/java/bni/regression/runners/WeekendJob.java
@@ -0,0 +1,14 @@
+package bni.regression.runners;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = { "json:src/test/resources/executionReports/cucumber/DBTestAutomationReport.json", "pretty", "html:src/test/resources/executionReports/cucumber/DBAutomationHTMLReport" },
+        features = {"src/test/resources/features"},
+        glue = {"bni.regression.steps.endToEndIntegrationSteps"})
+        //,tags = {"@smoke"})
+public class WeekendJob {
+
+}
