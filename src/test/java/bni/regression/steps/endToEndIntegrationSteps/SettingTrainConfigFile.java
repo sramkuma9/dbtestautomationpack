9e5388f2d374a0ddf5c06ca143f9654dd7711b52
shanthiathimuthu <ShanthiA@deliveryblueprints.com>
2020-03-05 14:51:45 +0530

    Train config file

---
+++ src/test/java/bni/regression/steps/endToEndIntegrationSteps/SettingTrainConfigFile.java
@@ -0,0 +1,44 @@
+package bni.regression.steps.endToEndIntegrationSteps;
+
+import bni.regression.libraries.common.CurrentDateTime;
+import bni.regression.libraries.common.ReadWritePropertyFile;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.TimeUnit;
+
+public class SettingTrainConfigFile {
+
+    private ReadWritePropertyFile readWritePropertyFile = new ReadWritePropertyFile();
+    private CurrentDateTime currentDateTime = new CurrentDateTime();
+
+    @Given("^Delete config file and copy train config file$")
+    public void searchFileAndDelete() {
+
+        Path sourceFilePath = Paths.get("src/test/resources/properties/trainConfig.properties");
+        Path targetFilePath = Paths.get("src/test/resources/properties/config.properties");
+        try {
+            Files.delete(targetFilePath);
+            TimeUnit.SECONDS.sleep(2);
+            Files.copy(sourceFilePath,targetFilePath);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @And("^^Copy testConfig file in the target path$$")
+    public void copyTestInputFile() throws IOException {
+        String[] dateSplit = currentDateTime.dateTime().split("/");
+        String timeSplit[] = dateSplit[2].split(" ");
+        String filePath = readWritePropertyFile.loadAndReadPropertyFile("testInputFilePath", "properties/config.properties");
+        Path sourceFilePath = Paths.get("src/test/resources/inputFiles/testInput.xlsx");
+
+        Path targetFilePath = Paths.get( filePath + dateSplit[0] + "/" + dateSplit[1] + "/" + timeSplit[0] + "/Track" + timeSplit[1].replaceAll(":","-")  + ".xlsx");
+
+        Files.copy(sourceFilePath,targetFilePath);
+    }
+}
---
+++ src/test/resources/features/SettingTrainConfigFile.feature
@@ -0,0 +1,6 @@
+
+Feature: Setting Train Config File
+
+  Scenario: Delete config file and copy train config file
+    Given Delete config file and copy train config file
+    And Copy testConfig file in the target path
